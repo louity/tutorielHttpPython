3576cb21c62fdbc50b56e84f0745b1bdf45806d8
Louis THIRY <louis.thiry@outlook.fr>
2016-05-10 11:48:32 +0200

    add comments

--- client.py
+++ client.py
@@ -1,22 +1,31 @@
+# une librairie pour faire des requetes HTTP
 import requests
 
-HOSTNAME = 'http://localhost:8003'
+# adresse du seveur
+HOSTNAME = 'A REMPLIR'
+# chemin des messages dans le serveur
 MESSAGES_PATH = '/messages'
 
 messages = ''
 
+# fonction qui demande l'ecriture d'un message dans la console
 def ecrireUnMessage () :
     message = raw_input('Entrez votre message : ')
     return message
 
+# fonction qui envoie le message au serveur
 def envoyerUnMessageAuServeur (message) :
     r = requests.post(HOSTNAME + MESSAGES_PATH, data = message)
 
+# fonction qui recupère tous les messafes du serveur
 def recupererLesMessagesDuServeur () :
+    global messages
     r = requests.get(HOSTNAME)
     messages = r.text
 
+# fonction qui affiche les messages dans la console
 def afficherLesMessages () :
+    global messages
     print(messages)
 
 message = ecrireUnMessage()
